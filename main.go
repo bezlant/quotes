5d3a877fc695729da7b41d8660a7b7d2a1a5054a
abezlyudniy <abezlyudniy@ozon.ru>
2025-05-26 12:25:11 +0700

    feat: add quotes

--- main.go
+++ main.go
@@ -21,6 +21,20 @@ func getAllQuotes(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(quotes)
 }
 
+func createQuote(w http.ResponseWriter, r *http.Request) {
+	var q Quote
+	// TODO: error handling
+	json.NewDecoder(r.Body).Decode(&q)
+
+	q.ID = nextID
+	nextID++
+	quotes = append(quotes, q)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(q)
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -30,6 +44,8 @@ func main() {
 	})
 	nextID++
 
-	http.ListenAndServe(":8080", nil)
 	r.HandleFunc("/quotes", getAllQuotes).Methods("GET")
+	r.HandleFunc("/quotes", createQuote).Methods("POST")
+
+	http.ListenAndServe(":8080", r)
 }
