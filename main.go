9918f6df230dc9fab608e9a99232cef8758466ed
abezlyudniy <abezlyudniy@ozon.ru>
2025-05-26 12:00:26 +0700

    feat: quotes handler

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type Quote struct {
+	ID    int    `json:"id"`
+	Quote string `json:"quote"`
+}
+
+var quotes []Quote
+var nextID = 1
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+	quotes = append(quotes, Quote{
+		ID:    nextID,
+		Quote: "Aboba abobcius abobenko",
+	})
+	nextID++
+
+	http.HandleFunc("/quotes", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(quotes)
 	})
 
 	http.ListenAndServe(":8080", nil)
