97557f9317853ee5ead2d15474479595b80ca151
abezlyudniy <abezlyudniy@ozon.ru>
2025-05-26 12:21:56 +0700

    feat: mux

--- main.go
+++ main.go
@@ -3,27 +3,33 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Quote struct {
-	ID    int    `json:"id"`
-	Quote string `json:"quote"`
+	ID     int    `json:"id"`
+	Author string `json:"author"`
+	Quote  string `json:"quote"`
 }
 
 var quotes []Quote
 var nextID = 1
 
+func getAllQuotes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(quotes)
+}
+
 func main() {
+	r := mux.NewRouter()
+
 	quotes = append(quotes, Quote{
 		ID:    nextID,
 		Quote: "Aboba abobcius abobenko",
 	})
 	nextID++
 
-	http.HandleFunc("/quotes", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(quotes)
-	})
-
 	http.ListenAndServe(":8080", nil)
+	r.HandleFunc("/quotes", getAllQuotes).Methods("GET")
 }
